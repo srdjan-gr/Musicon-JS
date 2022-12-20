bef4336b740c2e4f55b571c96cd4f9afaf978131
srdjan <srdjan.gr@gmail.com>
2022-12-20 09:23:59 +0100

    Search on enter. Footer add, pagination add.

--- app.js
+++ app.js
@@ -4,54 +4,99 @@ const searhEl = document.getElementById('search');
 const searchResaultEl = document.getElementById('searchResault');
 const searchBtnEl = document.getElementById('searchBtn');
 const cardEl = document.getElementById('card');
+const paginationEl = document.getElementById('pagination');
 
 let isLoading = false;
 
 
+window.addEventListener('load', () => {
+    showPagination();
+})
 
+
+// Searsh on Enter pres
+searhEl.addEventListener('keypress', async (e) => {
+
+    if (e.key === 'Enter') {
+        searchResaultEl.value = '';
+        cardEl.innerHTML = ''
+
+        isLoading = true
+        showSearchString();
+
+        const res = await fetch(`https://api.discogs.com/database/search?q=${searhEl.value}&key=${keys.key}&secret=${keys.sec}&per_page=10&page=1`)
+            .then((response) => response.json())
+            .then((data) => showSearcResault(data));
+    }
+});
+
+// Searsh on click
 searchBtnEl.addEventListener('click', async () => {
+
     searchResaultEl.value = '';
     cardEl.innerHTML = ''
 
     isLoading = true
     showSearchString();
 
-    const res = await fetch(`https://api.discogs.com/database/search?q=${searhEl.value}&key=${keys.key}&secret=${keys.sec}`)
+    const res = await fetch(`https://api.discogs.com/database/search?q=${searhEl.value}&key=${keys.key}&secret=${keys.sec}&per_page=10&page=1`)
         .then((response) => response.json())
         .then((data) => showSearcResault(data));
+
 });
 
 
+// Prikaz reci koja je pretrazivana
 const showSearchString = () => {
     searchResaultEl.value += `${searhEl.value}`;
 }
 
+
+// Prikaz rezultata pretrage
 const showSearcResault = (data) => {
 
     data.results.forEach(element => {
         cardEl.innerHTML += `
-            <div class="col">
-                <div class="card mb-4" style="width: 18rem;">
-                    <img src=${element.cover_image} class="card-img-top" style="height: 18rem; object-fit: cover; " alt="...">
+            <div class="col d-flex align-items-center justify-content-center">
+                <div class="card mb-4 " style="width: 18rem;">
+                    <img src=${element.cover_image} class="card-img-top" style="height: 20rem; object-fit: cover; " alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${element.title}</h5>
-                        <p class="card-text">Some quick example text to .</p>
                     </div>
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">An item</li>
-                        <li class="list-group-item">A second item</li>
-                        <li class="list-group-item">A third item</li>
+                        <li class="list-group-item">Release date: ${element.year}</li>
                     </ul>
                     <div class="card-body">
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+                        <a href=${element.resource_url} target="_blank" class="card-link">Relese details</a>
                     </div>
                 </div>
             </div>
         `
+        console.log(element);
     });
 
     searhEl.value = '';
+}
 
+
+//Prikaz Paginacie
+const showPagination = () => {
+    paginationEl.innerHTML = `
+        <nav aria-label="..." class="justify-content-center d-flex align-items-center">
+            <ul class="pagination ">
+                <li class="page-item ">
+                    <a class="page-link" href="#"><</a>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item active" aria-current="page">
+                    <span class="page-link">2</span>
+                </li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item">
+                    <a class="page-link" href="#">></a>
+                </li>
+            </ul>
+        </nav>
+    `
 }
 
